8798bc98da897bf236e7598b1fef0271b76fca28
Matt Crow <greengrappler12@gmail.com>
2021-03-09 12:11:39 -0800

    oops, forgot these

--- src/main/java/PreciousPhotographyShop/start/TestController.java
+++ src/main/java/PreciousPhotographyShop/start/TestController.java
@@ -1,17 +1,20 @@
 package PreciousPhotographyShop.start;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  *
  * @author Matt
  */
 
-@RestController
+@Controller
 public class TestController {
-    @RequestMapping("/")
-	public String index() {
-		return "If you can get this far, the project works!";
+    @GetMapping("/")
+	public String index(@RequestParam(name="name", required=false, defaultValue="Someone") String name, Model model) {
+        model.addAttribute("name", name);
+		return "myWebpage";
 	}
 }
