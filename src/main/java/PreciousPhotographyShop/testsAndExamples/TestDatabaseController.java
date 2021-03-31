0c712cededa3a1a76b24a8d12c2f4ebdea12546d
Matt Crow <greengrappler12@gmail.com>
2021-03-31 14:22:56 -0700

    not sure why the bridge table between EntityA and B isn't working. When doing the select statements myself, it should work, but the program isn't picking it up

--- src/main/java/PreciousPhotographyShop/testsAndExamples/TestDatabaseController.java
+++ src/main/java/PreciousPhotographyShop/testsAndExamples/TestDatabaseController.java
@@ -23,6 +23,9 @@ public class TestDatabaseController {
     @Autowired // Spring auto-generates this
     private TestRepository testRepository;
     
+    @Autowired private ARepo aRepo;
+    @Autowired private BRepo bRepo;
+    
     @PostMapping(path="/create") // Post, not get
     public @ResponseBody String createRecord(@RequestParam String message){
         // @ResponseBody means "I'm not returning the name of a view"
@@ -35,6 +38,36 @@ public class TestDatabaseController {
     @GetMapping(path="/list")
     public @ResponseBody Iterable<TestEntity> getAll(){
         return testRepository.findAll();
+    }
+    
+    @GetMapping(path="/map")
+    public @ResponseBody Iterable<EntityA> testMapping(){
+        aRepo.deleteAll();
+        bRepo.deleteAll();
+        
+        EntityA[] as = new EntityA[2];
+        EntityB[] bs = new EntityB[2];
+        for(int i = 0; i < 2; i++){
+            as[i] = new EntityA();
+            as[i] = aRepo.save(as[i]);
+            bs[i] = new EntityB();
+            bs[i] = bRepo.save(bs[i]);
+        }
+        Set<String> ids = new HashSet<>();
+        ids.add(as[0].getId());
+        ids.add(as[1].getId());
+        bs[0].setAIds(ids);
+        bs[0] = bRepo.save(bs[0]);
+        
+        ids = new HashSet<>();
+        ids.add(as[1].getId());
+        bs[1].setAIds(ids);
+        bs[1] = bRepo.save(bs[1]);
+        
+        aRepo.findAll().forEach(System.out::println);
+        bRepo.findAll().forEach(System.out::println);
+        
+        return aRepo.findAll();
     }
     
     @GetMapping(path="/main")
