fbf37e2bf6f40d9ee8870c320f8afbc437b08ea2
Matt Crow <greengrappler12@gmail.com>
2021-04-06 16:51:13 -0700

    added photo view capabilities

--- src/main/java/PreciousPhotographyShop/photographs/PhotographController.java
+++ src/main/java/PreciousPhotographyShop/photographs/PhotographController.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -80,5 +81,13 @@ public class PhotographController {
             ex.printStackTrace();
         }
         return ret;
+    }
+    
+    @GetMapping("/viewPhoto")
+    public String viewPhoto(@RequestParam String id, Model model){
+        PhotographEntity photo = this.databaseInterface.getPhotograph(id, true);
+        // todo error handling
+        model.addAttribute("photo", photo);
+        return "viewPhoto";
     }
 }
