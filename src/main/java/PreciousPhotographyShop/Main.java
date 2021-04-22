3f8a5b4b6fc4678e65321a6a5fd0598b96b52ed3
Daniel Ramos <rramsdany@gmail.com>
2021-04-22 09:54:26 -0700

    Emailing feature added.

--- src/main/java/PreciousPhotographyShop/Main.java
+++ src/main/java/PreciousPhotographyShop/Main.java
@@ -17,6 +17,6 @@ public class Main {
     
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
-        System.out.println("Program started");        
+        System.out.println("Program started");
     }
 }
