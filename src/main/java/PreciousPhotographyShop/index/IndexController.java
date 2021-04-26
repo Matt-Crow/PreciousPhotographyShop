0bd67faccb9bde705f21032afe097a59062146d4
Matt Crow <greengrappler12@gmail.com>
2021-04-26 12:50:32 -0700

    improved the search so that you can see search terms in the URL

--- src/main/java/PreciousPhotographyShop/index/IndexController.java
+++ src/main/java/PreciousPhotographyShop/index/IndexController.java
@@ -5,7 +5,12 @@ import PreciousPhotographyShop.categories.CategoryRepository;
 import PreciousPhotographyShop.databaseInterface.DatabaseInterface;
 import PreciousPhotographyShop.photographs.PhotographEntity;
 import PreciousPhotographyShop.photographs.PhotographRepository;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
@@ -34,32 +39,42 @@ public class IndexController {
         return "index-2";
     }
     
-    @PostMapping(value = "/search", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    /**
+     * 
+     * @param searchTerms the search terms the user entered in the search bar.
+     *  These are delimanated by spaces, so having searchTerms of "a b" counts
+     *  as searching "a" or "b" and finding the union of the results. May want to
+     *  change to intersection.
+     * 
+     * @param model
+     * 
+     * @return
+     * 
+     * @throws UnsupportedEncodingException if any issues arise from decoding
+     *  the search terms
+     */
+    @GetMapping(value = "/search")
     public String search(
-        @RequestBody MultiValueMap<String, String> formData,
+        @RequestParam(name="search-terms") String searchTerms,
         Model model
-    ){
-        formData.forEach((s, list)->{
-            System.out.println(s + ":");
-            list.forEach(System.out::println);
-        });
-        
+    ) throws UnsupportedEncodingException {
+        String[] terms = URLDecoder.decode(searchTerms, StandardCharsets.UTF_8.toString()).split(" ");
         LinkedList<SearchResultInfo> foundUrls = new LinkedList<>();
-        formData.get("search").forEach((String data)->{
-            catRepo.findAllByNameContainingIgnoreCase(data).forEach((cat)->{
+        
+        for(String term : terms){
+            catRepo.findAllByNameContainingIgnoreCase(term).forEach((cat)->{
                 foundUrls.add(resultFor(cat));
             });
-            photoRepo.findAllByNameContainingIgnoreCase(data).forEach((photo)->{
+            photoRepo.findAllByNameContainingIgnoreCase(term).forEach((photo)->{
                 foundUrls.add(resultFor(photo));
             });
-            photoRepo.findAllByDescriptionContainingIgnoreCase(data).forEach((photo)->{
+            photoRepo.findAllByDescriptionContainingIgnoreCase(term).forEach((photo)->{
                 foundUrls.add(resultFor(photo));
             });
             // todo add user search
             // review search?
-        });
+        }
         model.addAttribute("found", foundUrls);
-        
         return "search";
     }
     
