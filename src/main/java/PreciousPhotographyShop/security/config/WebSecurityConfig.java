6792b4715ad677fb3ef1f15b8de4a930d675f67f
Matt Crow <greengrappler12@gmail.com>
2021-04-29 12:32:14 -0700

    looks like login logout work!

--- src/main/java/PreciousPhotographyShop/security/config/WebSecurityConfig.java
+++ src/main/java/PreciousPhotographyShop/security/config/WebSecurityConfig.java
@@ -25,10 +25,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        // todo: probably should secure some of the API so nobody can delete users...
         http.csrf().disable().
             authorizeRequests().
-                antMatchers("/", "/index", "/search", "/allPhotos", "/api/registration/**", "/resources/**").permitAll().
-                anyRequest().authenticated().
+                //antMatchers("/", "/index", "/search", "/login", "/allPhotos", "/api/registration/**", "/resources/**").permitAll().
+                //anyRequest().authenticated().
+                antMatchers("/newPhoto").authenticated().
+                anyRequest().permitAll().
             and().
                 formLogin().permitAll().
             and().
