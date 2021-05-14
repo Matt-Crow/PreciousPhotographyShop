c91f038598f7562d2c2d0bfcd35eb427fee81a2e
Matt Crow <greengrappler12@gmail.com>
2021-05-14 14:40:17 -0700

    working on excluding app.prop from jar

--- src/main/java/PreciousPhotographyShop/Main.java
+++ src/main/java/PreciousPhotographyShop/Main.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Main {    
     public static void main(String[] args) {
+        todo check if application.properties exists
         SpringApplication.run(Main.class, args);
         System.out.println("Program started");
     }
