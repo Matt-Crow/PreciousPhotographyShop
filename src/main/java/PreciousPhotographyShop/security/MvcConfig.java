60dbdb2706cd624483503f55d668d1fc5b0a2ff2
danielvillavicencio <dany.villavicencio30@gmail.com>
2021-05-16 18:44:02 -0700

    Added social media buttons like in the homepage so it looks nicer

--- src/main/java/PreciousPhotographyShop/security/MvcConfig.java
+++ src/main/java/PreciousPhotographyShop/security/MvcConfig.java
@@ -6,11 +6,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
-
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("").setViewName("index-2");
         registry.addViewController("/").setViewName("index-2");
         registry.addViewController("/login").setViewName("login");
     }
-
 }
